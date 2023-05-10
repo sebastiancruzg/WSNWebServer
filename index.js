c9a4e2083169fb524f1b8edd72fbba34e8395627
sebastian cruz <sebasguti49@gmail.com>
2023-05-09 21:43:38 -0500

    firts commit

--- index.js
+++ index.js
@@ -18,21 +18,38 @@ const client = mqtt.connect(connectUrl, {
   reconnectPeriod: 1000,
 })
 
-const topic = 'enthu/260B8E5D/data'
+const topic0 = 'enthu/260B8E5D/data'
+const topic1 = 'enthu/260BF6C8/data'
 
 client.on('connect', () => {
   console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
+
+  client.subscribe([topic0], () => {
+    console.log(`Subscribe to topic '${topic0}'`)
   })
-  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+
+  client.subscribe([topic1], () => {
+    console.log(`Subscribe to topic '${topic1}'`)
+  })
+
+  client.publish(topic0, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
     if (error) {
       console.error(error)
     }
   })
+
+  client.publish(topic1, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+
 })
 
-let str;
+let nodo
+let temp
+let hum 
+let ppm
 
 client.on('message', (topic, payload) => {
   let result= "";
@@ -42,12 +59,13 @@ client.on('message', (topic, payload) => {
     let code = parseInt(payload.substr(i, 2), 16);
     result += String.fromCharCode(code);
   }
-  
+
   const array = result.split("/");
-  temp = array[1];
-  hum = array[2];
-  ppm = array[3];
-  console.log(temp,hum,ppm);
+  nodo = array[1];
+  temp = array[2];
+  hum = array[3];
+  ppm = array[4];
+  console.log(nodo,temp,hum,ppm);
 
 })
 
@@ -57,9 +75,9 @@ app.use(cors());
 app.get('/', (req, res) => {
 
     res.json({
+      "nodo": nodo,
       "temp": temp,
       "hum": hum,
-      "ppm": ppm
     }); 
 
 });
